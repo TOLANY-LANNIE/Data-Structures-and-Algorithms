78e02e6dd5d09d2965bf878ea7b83686afc4bc86
TOLANY-LANNIE <tolanym_2@live.com>
2021-11-01 18:03:28 +0200

    more DP problems added

---
+++ src/DynamicProgramming/CoinsProblem.java
@@ -0,0 +1,6 @@
+package DynamicProgramming;
+/*
+ * Given a set of coins, how can we make 27cents in the least number of coins
+ * */
+public class CoinsProblem {
+}
---
+++ src/DynamicProgramming/PotentPotProblem.java
@@ -0,0 +1,8 @@
+package DynamicProgramming;
+/*
+ * Given a set of substrings,
+ *  what are the possible ways to construct the
+ * string "potentpot'?
+ * */
+public class PotentPotProblem {
+}
