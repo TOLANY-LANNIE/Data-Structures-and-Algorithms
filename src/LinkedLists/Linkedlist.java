e3f4a732be0c3736e68a18048a30378f397647f0
TOLANY-LANNIE <tolanym_2@live.com>
2021-11-03 22:17:30 +0200

    commit linkedlist class

---
+++ src/LinkedLists/Linkedlist.java
@@ -0,0 +1,101 @@
+package LinkedLists;
+
+
+
+public class Linkedlist {
+    Node head;
+    public Linkedlist(){
+        this.head = null;
+    }
+    //Node class
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int val){
+            this.data = val;
+            this.next = null;
+        }
+    }
+    // Method to insert a new node
+    public static Linkedlist insert(Linkedlist list, int data)
+    {
+        // Create a new node with given data
+        Node new_node = new Node(data);
+        new_node.next = null;
+
+        // If the Linked List is empty,
+        // then make the new node as head
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            // Else traverse till the last node
+            // and insert the new_node there
+            Node curr = list.head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+
+            // Insert the new_node at last node
+            curr.next = new_node;
+        }
+
+        // Return the list by head
+        return list;
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(Linkedlist list)
+    {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        // Traverse through the LinkedList
+        while (currNode != null) {
+            // Print the data at current node
+            System.out.print(currNode.data + "->");
+
+            // Go to next node
+            currNode = currNode.next;
+        }
+    }
+
+    //check if val is in the linkedlist
+    private static boolean contains(Linkedlist list, int target){
+        Node currNode = list.head;
+        // Traverse through the LinkedList
+        while (currNode != null) {
+            // Go to next node
+            if(currNode.data ==target) return true;
+
+            currNode = currNode.next;
+        }
+        return false;
+    }
+    // Driver code
+    public static void main(String[] args)
+    {
+        /* Start with the empty list. */
+        Linkedlist list = new Linkedlist();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        list = insert(list, 6);
+        list = insert(list, 7);
+        list = insert(list, 8);
+
+        // Print the LinkedList
+       // printList(list);
+        System.out.println(contains(list, 4));
+    }
+}
---
+++ src/LinkedLists/RecursiveLinkedList.java
@@ -0,0 +1,182 @@
+package LinkedLists;
+
+public class RecursiveLinkedList {
+    static Node head;
+
+    public RecursiveLinkedList(){
+        this.head = null;
+    }
+
+    //Node class
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int val){
+            this.data = val;
+            this.next = null;
+        }
+    }
+
+    // Function to insert a new node at the
+// end of linked list using recursion.
+    static Node insertEnd(Node head, int data)
+    {
+        // If linked list is empty, create a
+        // new node (Assuming newNode() allocates
+        // a new node with given data)
+        if (head == null){
+            head = new Node(data);
+            return head;
+        }
+            // If we have not reached end, keep traversing
+            // recursively.
+        else
+            head.next = insertEnd(head.next, data);
+        return head;
+    }
+
+    static void traverse(Node head)
+    {
+        if (head == null)
+            return;
+
+        // If head is not null, print current node
+        // and recur for remaining list
+        System.out.print( head.data + "-> ");
+
+        traverse(head.next);
+    }
+    static boolean contains(Node head, int target){
+        if(head ==null) return false;
+
+        if(head.data==target) return true;
+
+        return contains(head.next,target);
+    }
+    static int sumList(Node head){
+        /* // iterative solution
+        int sum =0;
+        Node curr = head;
+        while (curr!=null){
+            sum+=curr.data;
+            curr =curr.next;
+        }*/
+
+        //recursively
+        if(head==null) return 0;
+        return head.data + sumList(head.next);
+    }
+
+    static Node delete(Node head, int target){
+        if(head.data==target) {
+            return head.next;
+        }
+
+        Node prev = null;
+        Node curr = head;
+
+         while(curr!= null){
+             if(curr.data ==target){
+                 //delete node
+                 prev.next = curr.next;
+             }
+             prev = curr;
+             curr = curr.next;
+         }
+         return head;
+    }
+
+    static Node deleteRecursively(Node head, int target){
+
+        // If linked list is empty
+        if (head == null)
+            return null;
+
+        // Base case (start needs to be deleted)
+        if (head.data == target)
+        {
+            Node res = head.next;
+            return res;
+        }
+
+        head.next = deleteRecursively(head.next, target);
+        return head;
+    }
+    static Node deleteNodeNth(Node start, int k)
+    {
+        // If invalid k
+        if (k < 1)
+            return start;
+
+        // If linked list is empty
+        if (start == null)
+            return null;
+
+        // Base case (start needs to be deleted)
+        if (k == 1)
+        {
+            Node res = start.next;
+            return res;
+        }
+
+        start.next = deleteNodeNth(start.next, k-1);
+        return start;
+    }
+
+    static Node reserveLinkedList(Node head){
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return  head;
+    }
+    static Node reserveLinkedListRecursive(Node head){
+        if(head == null) {
+            return head;
+        }
+
+        // last node or only one node
+        if(head.next == null) {
+            return head;
+        }
+
+        Node newHeadNode = reserveLinkedListRecursive(head.next);
+
+        // change references for middle chain
+        head.next.next = head;
+        head.next = null;
+
+        // send back new head node in every recursion
+        return newHeadNode;
+    }
+    // Driver code
+    public static void main(String args[])
+    {
+        Node head = null;
+        head = insertEnd(head, 6);
+        head = insertEnd(head, 8);
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 12);
+        head = insertEnd(head, 14);
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 13244);
+        traverse(head);
+
+        //System.out.println("\n Contains "+contains(head,12));
+        //System.out.println("\n Sum "+sumList(head));
+        //deleteRecursively(head, 15);
+        System.out.println("\n=========================");
+        //deleteNodeNth(head,3);
+        Node newHead =reserveLinkedListRecursive(head);
+        traverse(newHead);
+
+
+    }
+}
