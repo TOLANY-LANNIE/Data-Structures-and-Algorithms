5c52e303b7aec19692aff827af02aeb4875f9f3a
TOLANY-LANNIE <tolanym_2@live.com>
2021-11-08 00:45:46 +0200

    commit

--- src/Arrays/Kth_Largest.java
+++ src/Arrays/Kth_Largest.java
@@ -7,7 +7,7 @@ public class Kth_Largest {
     public static void main(String[] args) {
         int [] array = {1,2,8,9,5,6,7};
         int k=2; //end (k is the 6th largest)
-        int x=4; //start (x is the 4th largest)
+        int x=6; //start (x is the 4th largest)
         findLargest(array,x,k ); //time complexity 0(N)
     }
    static void findLargest(int []array, int x, int k ){
